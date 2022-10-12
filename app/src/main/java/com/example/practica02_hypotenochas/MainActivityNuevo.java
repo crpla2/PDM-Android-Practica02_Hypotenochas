efa3734d738bcf4704ec9c71b9fe563d573cd341
crpla2 <210176@iessierradeguara.com>
2022-10-12 03:35:16 +0200

    añado java buscaminas

--- app/src/main/java/com/example/practica02_hypotenochas/MainActivityNuevo.java
+++ app/src/main/java/com/example/practica02_hypotenochas/MainActivityNuevo.java
@@ -10,7 +10,19 @@ public class MainActivityNuevo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_personajes);
-        entrada = getIntent();
+        setContentView(R.layout.activity_main_nuevo);
+        entrada= getIntent();
     }
+
+    /*
+    		int casillas = 64;
+		int numMinas = 10;
+		int posicion = 15;
+		int filasTablero = 8;
+
+		buscaminas juego= new buscaminas(casillas, numMinas, posicion, filasTablero);
+		juego.generaTablero(casillas, numMinas);
+		System.out.println(juego.descubreCasillas(posicion, filasTablero));
+		System.out.println(juego.descubreCasillas(64, filasTablero));
+     */
 }
